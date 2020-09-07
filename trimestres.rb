0ddb2aef72d242c7d74a75535d527c00021ed32e
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-07 12:21:29 -0300

    erasing comments

--- trimestres.rb
+++ trimestres.rb
@@ -1,15 +1,3 @@
-# Crear el programa trimestres.rb donde se pide generar un hash llamado quarters con las ventas
-# de cada trimestre a partir del hash de ventas de los enunciados anteriores. Las claves del hash tienen
-# que ser 'Q1', 'Q2', 'Q3', 'Q4'
-# Uso:
-# ruby trimestres.rb
-# {"Q1"=>49000, "Q2"=>111000, "Q3"=>87200, "Q4"=>133500}
-# Tips:
-# Los valores ingresados serán distintos
-# Es un ejercicio de arrays o de hashes ¿Se necesitan los keys?
-# Revisar la documentación del método .each_slice
-# Necesitamos iterar elementos, o elementos con índices
-
 ventas = {
     Enero: 15000,
     Febrero: 22000,
